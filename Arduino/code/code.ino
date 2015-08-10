bdcfcf6ddc55332f1c313aa814ce0da62c0efd7b
Minhaz A V <minhazav@gmail.com>
2015-08-10 23:36:04 +0530

    Arduino code to send back ultrasonic sensor data added

---
+++ Arduino/code/code.ino
@@ -0,0 +1,27 @@
+// ---------------------------------------------------------------------------
+// Example NewPing library sketch that does a ping about 20 times per second.
+// ---------------------------------------------------------------------------
+
+#include <NewPing.h>
+
+#define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+#define LEDPIN 13
+#define DDELTA 10
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+void setup() {
+  Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
+  pinMode(LEDPIN, OUTPUT);
+}
+
+void loop() {
+  delay(30);                      // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  unsigned int uS = sonar.ping(); // Send ping, get ping time in microseconds (uS).
+  Serial.println(uS / US_ROUNDTRIP_CM); // Convert ping time to distance in cm and print result (0 = outside set distance range)
+  if (uS / US_ROUNDTRIP_CM <= 10) digitalWrite(LEDPIN, HIGH);
+  else digitalWrite(LEDPIN, LOW);
+}
